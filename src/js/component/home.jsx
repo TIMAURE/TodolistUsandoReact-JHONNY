f6c6f1f0c95f7593d6bf5a43c4b727a279e63bb5
Jhonny Timaure <164609178+TIMAURE@users.noreply.github.com>
2024-06-14 16:20:53 +0000

    se agrego onCharge y Onclick y algo de css,continuar con CSs

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { UsageState } from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+const[inputvalue, setinputvalue] = useState ("")
+const[todo,settodo] = useState([])
+
 	return (
 		<div className="contenerdo">
 <h1>todos</h1>
 <ul>
-	<li><input type="text" placeholder="Que necesitas hacer?"></input></li>
-	<li>pasear al perro<i class="fa-solid fa-x"></i></li>
-	<li>comer<i class="fa-solid fa-x"></i></li>
+	<li><input type="text" placeholder="Que necesitas hacer?" onChange={(e)=> setinputvalue(e.target.value)} value={inputvalue} onKeyDown={(e) =>{if (e.key === "Enter"){settodo(todo.concat([inputvalue]));setinputvalue("");}} } ></input></li>
+{todo.map((item, index) =><li>{item}{""}<i class="fa-solid fa-x" onClick={() => settodo(todo.filter((t, currentindex)=> index != currentindex))}></i></li>
+)}
 </ul>
-<div>2 Items</div>
+
+<div className="numeroPagina">{todo.length} items</div>
 
 
 		</div>
