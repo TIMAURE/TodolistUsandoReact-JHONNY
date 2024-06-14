70cc0dad9e151085856618135a99a5a2536e9905
Jhonny Timaure <164609178+TIMAURE@users.noreply.github.com>
2024-06-14 18:33:42 +0000

    correccion de Error en Input

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-const[inputValue, setInputValue] = useState ("")
-const[todo,settodo] = useState([])
+const[inputValue, setInputValue] = useState ("");
+const[todo,settodo] = useState([]);
 
 	return (
 		<div className="contenedo">
 <h1>To Do!</h1>
 <ul>
-	<li><input type="text" placeholder="Tareas Por Hacer?" onChange={(e)=> setInputValue(e.target.value)} value={inputValue} onKeyDown={(e) =>{if (e.key === "Enter"){settodo(todo.concat([inputValue]));setinputvalue("");}} } ></input></li>
+	<li><input type="text" placeholder="Tareas Por Hacer?" onChange={(e)=> setInputValue(e.target.value)} value={inputValue} onKeyDown={(e) =>{if (e.key === "Enter"){settodo(todo.concat([inputValue]));setInputValue("");}} } ></input></li>
 	{todo.length === 0 ? (
           <li>"No hay tareas, añadir tareas"</li>
         ) :todo.map((item, index) =><li>{item}{""}<i className="fa-solid fa-x" onClick={() => settodo(todo.filter((t, currentindex)=> index != currentindex))}></i></li>
