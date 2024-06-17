c41fad6549e7615037bfe2951eaaee4552ab7cc1
Jhonny Timaure <164609178+TIMAURE@users.noreply.github.com>
2024-06-17 19:24:33 +0000

    correccionHoverFinal

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,18 +8,22 @@ const[todo,settodo] = useState([]);
 
 	return (
 		<div className="contenedo">
-<h1>To Do!</h1>
+<h1>To Do List!</h1>
 <ul>
-	<li><input type="text" placeholder="Tareas Por Hacer?" onChange={(e)=> setInputValue(e.target.value)} value={inputValue} onKeyDown={(e) =>{if (e.key === "Enter"){settodo(todo.concat([inputValue]));setInputValue("");}} } ></input></li>
-	{todo.length === 0 ? (
-          <li>"No hay tareas, añadir tareas"</li>
-        ) :todo.map((item, index) =><li>{item}{""}<i className="fa-solid fa-x" onClick={() => settodo(todo.filter((t, currentindex)=> index != currentindex))}></i></li>
-)}
+	<li><input type="text" 
+	placeholder="Tareas Por Hacer?" 
+	onChange={(e)=> setInputValue(e.target.value)} 
+	value={inputValue} 
+	onKeyDown={(e) =>{if (e.key === "Enter"){settodo(todo.concat([inputValue]));
+	setInputValue("");}} } ></input>
+	</li>
+	{todo.length === 0 ? (<li>"No hay tareas, añadir tareas"</li>) : todo.map((item, index) =><li className="conjunto">{item}{""}
+		<i className="fa-solid fa-x" 
+		onClick={() => 
+		settodo(todo.filter((t, currentindex)=> index != currentindex))}>
+			</i></li>)}
 </ul>
-
-<div className="numeroPagina">{todo.length} items</div>
-</div>
-	);
-};
+<div className="numeroPagina"><strong>{todo.length} items</strong></div>
+</div>);};
 
 export default Home;
